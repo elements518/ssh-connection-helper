88f25823826be35f4ce4f5fdf44ad0781b3a63b1
Max <elements@elementsmail.ddns.net>
2018-11-23 10:40:25 +0100

    Update ssh-connection-helper.sh
    Virtual "folder" for bookmarks

--- ssh-connection-helper.sh
+++ ssh-connection-helper.sh
@@ -1,20 +1,40 @@
 #!/bin/bash
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
-#echo $SCRIPTPATH/tmp
-file=$SCRIPTPATH/list
+
+ls Scripte/bookmarks/ | sed -E -e 's/[[:blank:]]+/\n/g' >> $SCRIPTPATH/tmp2
+file2=$SCRIPTPATH/tmp2
+counter2=1
+while read d
+do
+			echo "Number: "$counter2 "Name: "$d >> $SCRIPTPATH/tmp3
+			let counter2++
+done < $file2
+cat $SCRIPTPATH/tmp3
+echo
+read -p "Customer Number : " chosen
+echo
+list="$(cat $SCRIPTPATH/tmp3 | grep " $chosen " | awk -F ':' '{print $3}' | sed -e 's/ //g')"
+rm -rf $SCRIPTPATH/tmp*
+
+
+
+file=$SCRIPTPATH/bookmarks/$list
 counter=1
 while read p
-        do
-                        echo "Number: "$counter "Name: "$p >> $SCRIPTPATH/tmp
-                        let counter++
-        done < $file
-cat $SCRIPTPATH/tmp
+	do
+			echo "Number: "$counter "Name: "$p >> $SCRIPTPATH/bookmarks/tmp
+			let counter++
+	done < $file
+cat $SCRIPTPATH/bookmarks/tmp
 echo
-echo "Which Server do you want to connect to?"
-read ssh
-echo "Which User do you want to use?"
-read user
-ssh20="$(cat "$SCRIPTPATH"/tmp | grep " ${ssh} " | awk -F ':' '{print $3}' | sed -e 's/ //g')"
-rm $SCRIPTPATH/tmp
-#echo $ssh20
+read -p "Server Number : " ssh
+echo
+read -e -p "Username : " -i "root" user
+echo $user
+
+
+
+ssh20="$(cat "$SCRIPTPATH"/bookmarks/tmp | grep " ${ssh} " | awk -F ':' '{print $3}' | sed -e 's/ //g')"
+echo $ssh20
+rm $SCRIPTPATH/bookmarks/tmp
 ssh -l $user -i ~/.ssh/id_rsa $ssh20
